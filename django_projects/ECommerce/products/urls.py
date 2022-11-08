2bd212a7e1a2bd1290f1c59d936fe4754262e0d1
gaurav surolia <grvsurolia@gmail.com>
2022-11-08 12:48:24 +0530

    ecomm single product

--- django_projects/ECommerce/products/urls.py
+++ django_projects/ECommerce/products/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path("",views.home, name="home"),
+    path("category/<int:category_id>/",views.Category_products, name="category_products"),
+    path("product/<int:product_id>/",views.single_product, name="single_product"),
 ]
--- django_projects/ECommerce/products/views.py
+++ django_projects/ECommerce/products/views.py
@@ -2,6 +2,21 @@ from django.shortcuts import render
 from .models import *
 
 def home(request):
+    all_categories = Category.objects.all()
     all_products = Product.objects.all()
-    context = {"products":all_products}
+    context = {"products":all_products, "categories":all_categories}
     return render(request, "home.html", context)
+
+
+def Category_products(request, category_id):
+    all_categories = Category.objects.all()
+    categoryOb = Category.objects.get(id = category_id)
+    products = Product.objects.filter(category = categoryOb)
+    context = {"products":products, "categories":all_categories}
+    return render(request, "category_product.html", context)
+
+def single_product(request, product_id):
+    all_categories = Category.objects.all()
+    productOb = Product.objects.get(id=product_id)
+    context = {"product":productOb,"categories":all_categories}
+    return render(request, "single_product.html", context)
