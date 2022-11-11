325ca7f22932c692f3b690faaed763b1aa7e6b6c
gaurav surolia <grvsurolia@gmail.com>
2022-11-11 17:45:48 +0530

    ecomm

--- django_projects/ECommerce/products/urls.py
+++ django_projects/ECommerce/products/urls.py
@@ -5,4 +5,7 @@ urlpatterns = [
     path("",views.home, name="home"),
     path("category/<int:category_id>/",views.Category_products, name="category_products"),
     path("product/<int:product_id>/",views.single_product, name="single_product"),
+    path("register/", views.register_view, name="register"),
+    path("login/", views.login_view, name="login"),
+    path("logout/", views.logout_view, name="logout"),
 ]
--- django_projects/ECommerce/products/views.py
+++ django_projects/ECommerce/products/views.py
@@ -1,6 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import *
+from django.contrib import messages
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 
+
+@login_required(login_url="/login/")
 def home(request):
     all_categories = Category.objects.all()
     all_products = Product.objects.all()
@@ -8,6 +14,7 @@ def home(request):
     return render(request, "home.html", context)
 
 
+@login_required(login_url="/login/")
 def Category_products(request, category_id):
     all_categories = Category.objects.all()
     categoryOb = Category.objects.get(id = category_id)
@@ -15,8 +22,54 @@ def Category_products(request, category_id):
     context = {"products":products, "categories":all_categories}
     return render(request, "category_product.html", context)
 
+
+@login_required(login_url="/login/")
 def single_product(request, product_id):
     all_categories = Category.objects.all()
     productOb = Product.objects.get(id=product_id)
     context = {"product":productOb,"categories":all_categories}
     return render(request, "single_product.html", context)
+
+
+def register_view(request):
+    if request.method == "POST":
+        print("Register")
+        post_data = request.POST
+        print(post_data)
+        name = post_data["name"]
+        email = post_data["email"]
+        password1 = post_data["password1"]
+        password2 = post_data["password2"]
+
+        if password1 != password2:
+            messages.error(request, "Password Not Match")
+        else:
+            userOb = User.objects.create_user(username=name, email=email, password=password1)
+            messages.success(request, "Saved Successfully")
+        return redirect("/register/")
+
+    else:
+        return render(request, "register.html")
+
+
+def login_view(request):
+    if request.method == "POST":
+        print("login")
+        post_data = request.POST
+        user = authenticate(username=post_data["username"], password=post_data["password"])
+        if user == None:
+            messages.error(request, "Username or Password is incorrect")
+            return redirect("/login/")
+        else:
+            login(request, user)
+            messages.info(request, "Logged in successfully")
+            return redirect("/")
+
+    else:
+        return render(request, "login.html")
+
+
+def logout_view(request):
+	logout(request)
+	messages.info(request, "You have successfully logged out.") 
+	return redirect("/login/")
