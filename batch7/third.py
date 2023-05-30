01b8961533da585c3cca981ba90c0df190de6257
Gaurav <grvsurolia@gmail.com>
2023-05-30 09:07:15 +0530

    batch 7 3

---
+++ batch7/third.py
@@ -0,0 +1,86 @@
+# ## String
+
+# a = "abce dfgh"
+# print(a, type(a))
+
+# print(len(a))
+
+# b = '''Welcome! Are you completely new to programming? If not then we presume you will be looking for information about why 
+# and how to get started with Python. Fortunately an experienced programmer in any programming language (whatever it may be) 
+# can pick up Python very quickly. It's also easy for beginners to use and learn, so jump in! Welcome! Are you completely new 
+# to programming? If not then we presume you will be looking for information about why and how to get started with Python. 
+# Fortunately an experienced programmer in any programming language (whatever it may be) can pick up Python very quickly. 
+# It's also easy for beginners to use and learn, so jump in!'''
+
+# c = """Welcome! Are you completely new to programming? If not then we presume you will be looking for information about 
+# why and how to get started with Python. Fortunately an experienced programmer in any programming language (whatever it may be) 
+# can pick up Python very quickly. It's also easy for beginners to use and learn, so jump in!
+# Welcome! Are you completely new to programming? If not then we presume you will be looking for information about why and how to 
+# get started with Python. Fortunately an experienced programmer in any programming language (whatever it may be) can pick up 
+# Python very quickly. It's also easy for beginners to use and learn, so jump in!"""
+
+# print(b)
+# print(len(b))
+
+########################################################################################################
+
+# in operator
+
+# a = "my name is gaurav"
+# print("anisha" in a)
+# print("gaurav" not in a)
+
+###########################################################################################################
+
+## Slicing
+
+# a = "my name is gaurav"
+
+# print(a)
+
+# print(a[0])
+# print(a[16])
+# print(a[17])
+
+# print(a[start:end+1])
+# print(a[0:7])
+# print(a[0:9])
+# print(a[3:7])
+# print(a[11:20])
+
+# a = "my name is gaurav"
+
+# print(a[-1])
+# print(a[-6])
+# print(a[-14:-10])
+# print(a[-9:-7])
+# print(a[-6:])
+
+# a = "gaurav"
+# print(a.upper())
+
+# b = "GAURAV"
+# print(b.lower())
+
+# c = "gaUrAV"
+# print(c.title())
+
+# a = "anisha"
+# b = "robin"
+# print(a + " " + b)
+
+# a = "robin"
+# print(a*3)
+# b = 3
+# print(a*b)
+# print(a + b)  # Not Possible
+
+# age = 22
+# s = "My age is {}, I am a {}"
+# print(s.format(age, "Developer"))
+
+
+# a = "Welcome! Are you completely new to programming?\tIf not then we presume you will be looking for information about why and how to get started with Python.\n Fortunately an experienced programmer in any programming language (whatever it may be) can pick up Python very quickly.\n It's also easy for beginners to use and learn, so jump in!"
+# print(a)
+
+##################################################################################################
