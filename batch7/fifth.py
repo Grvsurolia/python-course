afc94832c1b9457bb20589211355cc69c393b70d
Gaurav <grvsurolia@gmail.com>
2023-06-04 11:25:27 +0530

    batch7 list functions

---
+++ batch7/fifth.py
@@ -0,0 +1,134 @@
+## List Function
+
+# l = [1,2,3,4,5,'a','b',6,7,8,9]
+
+# print(l)
+# print(l[5])
+# l.append(10)
+# l.append(11)
+
+# l[5] = 'aa'
+
+# p = [11,22,33,44,55]
+# l.append(p)
+# l.extend(p)
+# l = [1,2,3,4,5,'a','b',6,4,7,4,8,9]
+# print(l)
+
+# l.insert(7, 'c')
+
+# l.pop()
+# l.pop(5)
+
+# l.remove(4)
+# l.remove(4)
+
+# c = l.count(4)
+
+# l.sort()
+
+# print(l)
+
+# CRUD -> Create Read Update Delete
+
+#################
+
+# Tuple Functions
+
+# t = (1,2,3,4,'a','b',5,6,4,7,21,8,9)
+
+# print(t.count('a'))
+# t.sort()        # Not possible
+# print(t)
+
+## Set Function
+
+# s = {1,2,3,4,'a','b',5,6,7,4,3,2}
+# print(s.count(2))     # Not possible
+
+## Dictionary Functions
+
+# d1 = {
+#     "name":"gaurav",
+#     "class":2,
+#     "age":3
+#     }
+# print(d1)
+# # print(d["name"])
+# d2 = {
+#     "roll_no":7,
+#     "subject":"python"
+# }
+
+# d1.update(d2)
+# print(d1)
+
+###############################
+
+# Enter your Name: Gaurav
+# Welcome Gaurav
+
+# name = input("Enter your Name: ")
+# print(f"Your Name is {name}")
+
+##################################
+
+# Enter your age: 22
+# Your age is 22
+
+# age=input("enter your age: ")
+# print(f"My age is {age}")
+
+###################################
+
+# Enter your name: Gaurav
+# 6
+
+# robinpreet
+# 10
+
+# name = input("Enter your name: ")
+# print(len(name))
+
+######################################
+
+# Enter a string
+# Hello, my name is gaurav, I am a developer
+# 5
+
+# s = input("Enter a string\n")
+# print(s.count("a"))
+
+#######################################
+
+# Enter your Name: gaurav
+# Your Name starts with G and ends with V
+
+# name = input("Enter your Name: ")
+# start_name = name[0].upper()
+# end_name = name[-1].upper()
+
+# print(f"Your Name starts with {start_name} and ends with {end_name}")
+
+###########################################
+
+# l = ["aaa","bbb","ccc", "ddd", "eee"]
+
+# ["aaa","BBB","dd", "Eee"]
+
+# l = ["aaa","bbb","ccc", "ddd", "eee"]
+# l[1] = l[1].upper()
+# l.remove("ccc")
+# l[2] = 'dd'
+# l[3] = l[3].title()
+# print(l)
+#########################################
+
+# l = [1,2,3,4,5,6,7,8,9,0]
+# print(l * 10)
+# p = [1,2,3,4]
+# print(l - p)
+# print(l+p)
+
+#########################################
+
