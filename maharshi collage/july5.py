cd3bb4586d15239bbde2bd9bda539a73b703f083
Gaurav <grvsurolia@gmail.com>
2023-06-05 09:07:46 +0530

    maharshi for loop

---
+++ maharshi collage/july5.py
@@ -0,0 +1,70 @@
+## Loops
+
+## For loop syntax
+
+# for var in iterator:
+#     statement
+
+# l = [1,2,3,4,5,6,7,8,9]
+
+# for i in l:
+#     print(i)
+####################
+# for i in range(0,11):
+#     print(i)
+####################
+
+# for i in range(0,11,2):
+#     print(i)
+
+####################
+
+# range syntax 
+# range(start, end+1, steps)
+
+######################
+
+# for i in range(1,11):
+#     print(i*2)
+
+# for i in range(2,21,2):
+#     print(i)
+
+#####################
+
+# n = int(input("Enter a no: "))
+
+# for i in range(1,11):
+#     print(i*n)
+
+# for i in range(n, (n*10)+1, n):
+#     print(i)
+
+######################################
+
+# l = ["kartik", "pawan", "mann", "nisha", "aman"]
+
+# counter = 1
+# for i in l:
+#     print(counter, "=", i)
+#     counter = counter + 1
+
+# 1 = Kartik
+# 2 = pawan 
+# 3 = mann 
+# 4 = nisha
+# 5 = aman 
+
+
+
+##################################################
+
+# l = [2,3,5,7,9,33,55,77]
+
+# Output: [3,4,6,8,10,34,56,78]
+
+###################################################
+
+# l = [2,3,4,4,5,6,6,7,78,8,9,99]
+
+# output: 12
