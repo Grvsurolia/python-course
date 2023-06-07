2cf351b3dc5620b751c92fae5be6694cd86c4294
Gaurav <grvsurolia@gmail.com>
2023-06-07 08:26:55 +0530

    maharshi assignment questions

---
+++ maharshi collage/july6.py
@@ -0,0 +1,92 @@
+# l = [2,3,5,7,9,33,55,77]
+# for i in range(0,len(l)):
+#     l[i] = l[i] + 1
+# print(l)
+
+# Output: [3,4,6,8,10,34,56,78]
+
+###################################################
+
+# l = [2,3,4,4,5,6,6,7,78,8,9,99]
+# count = 0
+# for i in l:
+#     count = count + 1
+# print(count)
+
+# output: 12
+
+# n = int(input("Enter a number: "))
+
+# if n<0:
+#     print("Negative")
+# else:
+#     print("Positive")
+
+###########################################
+
+# Enter no: 2
+# Enter no: 3
+# Enter no: 4
+# Enter no: 5
+# Enter no: 6
+# [2,3,4,5,6]
+
+# n1 = int(input("Enter No: "))
+# n2 = int(input("Enter No: "))
+# n3 = int(input("Enter No: "))
+# n4 = int(input("Enter No: "))
+# n5 = int(input("Enter No: "))
+# print([n1,n2,n3,n4,n5])
+
+# l = []
+# for i in range(0,5):
+#     n = int(input("Enter No: "))
+#     l.append(n)
+# print(l)
+
+#####################################
+
+# Enter no: 22
+# Enter no: 33
+# Enter no: 44
+# Enter no: 55
+# Enter no: 66
+# [23,34,45,56,67]
+
+# l = []
+# for i in range(0,5):
+#     n = int(input("Enter No: "))
+#     l.append(n+1)
+# print(l)
+
+#############################################
+
+# d = {
+#     "name":"Gaurav",
+#     "age":2,
+#     "class":3,
+#     "course":"Python"
+# }
+
+# output: "Your name is Gaurav, Your age is 2, Your class is 3, Your course is python"
+##############################################
+
+# Enter Your Name: Gaurav
+# Enter Your Age: 22
+# Enter Your Class: 7
+# Enter Your Course: Python
+
+# Output: 
+# {
+#     "name":"Gaurav",
+#     "age":22,
+#     "class":7,
+#     "course":"Python"
+# }
+
+###########################################
+
+# t = ("abhishek", "ankit", "aman", "arpit", "divyansh")
+
+# output: 
+# ("abhishek1", "ankit2", "aman3", "arpit4", "divyansh5")
