2d6c1edb5e3c637e50282024ad2af478a1ebcb2d
gaurav surolia <grvsurolia@gmail.com>
2024-06-25 22:15:44 +0530

    batch anisha added, env deleted

---
+++ batch anisha/first.py
@@ -0,0 +1,25 @@
+# Comments
+# 1. single line 
+# 2. Multi line comment
+
+'''My name is Anisha, 
+I am a Developer
+I live in india'''
+
+
+"""My name is Anisha, 
+I am a Developer
+I live in india"""
+
+# My name is Anisha, 
+# I am a Developer
+# I live in india
+
+# print("Gaurav Surolia", end=" ")
+# print("Anisha Saini", end="#")
+
+# print("anisha","saini", 20)
+# print("anisha saini 20")
+
+# print("anisha","saini", 20, sep="*")
+
