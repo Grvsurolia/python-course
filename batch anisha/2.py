554cf428f10d69a3659505040f50c295f4ec93cf
gaurav surolia <grvsurolia@gmail.com>
2024-06-26 21:57:00 +0530

    2.py added

---
+++ batch anisha/2.py
@@ -0,0 +1,118 @@
+######### VARIABLES
+
+# a = 2
+# print("first a = ",a)
+
+# a = 3
+# print("second a = ",a)
+
+# s = "I am from jaipur"
+# print(s)
+
+########## Data types
+
+# Integer int 2, 33,167, 7567645735
+# Decimal float 3.14, 0.1, 1.1
+# String str 'gaurav', "Anisha", 'My name is gaurav, I live in jaipur, I am a developer.'
+# Bool bool True / False
+
+# Sequential 
+#     List list [1,2,3,4,"a","b","C", "D",2.22]
+#     Tuple tuple (1,2,3,4,"a","b","C", "D",2.22)
+#     Set set {1,2,3,4,"a","b","C", "D",2.22}
+
+# Mapping 
+#     Dictionary dict {key1: value1, key2:value2, ..........}   keys should be unique
+
+# a = 2
+# print(a)
+# print(type(a))
+
+# f = 2.2
+# print(f, type(f))
+
+# s = "sfjdhb sdfbgkjfbghjb kjdbjd kdbjk fdb kjbgsjk"
+# s = 'sf hjsdbvhjfsbhjsdb hjsvhjfskgsfhvbhjfdbvywra'
+# s = '''Python is a high-level programming language in computer science. One of the main 
+# features 
+# of Python is that the written program is easy to read and understand. It was written in 1991 
+# under the direction of Guido van Ross. Python also has a design philosophy that emphasizes 
+# code readability. '''
+# print(s, type(s))
+
+# b = False
+# print(b, type(b))
+
+######## List, tuple, set
+
+# l = [1,2,3,4,5,6,7,8,9,10]    # Mutable   # Ordered
+
+# print(l, type(l))
+
+# t = (1,2,3,4,5,6,7,8,9,10)    # Immutable     # Ordered
+# print(t, type(t))
+
+# s = {1,2,3,4,5,6,7,8,9,10}    # mmutable  # Non subscriptable  # Unordered    # Stores unique values
+# print(s, type(s))
+
+######## Dictionary
+
+# d = {"a":1, "b":2, "c":3}
+# print(d, type(d))
+
+######################################################################
+
+############### Difference b/w List Tuple and Set 
+
+# indexing starts fron 0 and end at length-1
+# Negative indexing starts from -1 and end at -length
+
+# l = [1,2,3,4,5,6,7,8,9,10,10]
+# print(l)
+# print(l[0])
+# print(l[5])
+
+# print(l[-1])
+# print(l[-2])
+# print(l)
+# l[1] = 22
+# l[7] = 88
+# print(l)
+##########################
+
+# t = (1,2,3,4,5,6,7,8,9,10,11,12)
+# # print(t[1])
+# # print(t[-2])
+
+# print(t)
+
+# t[1] = 22
+# print(t)
+
+##########################
+
+# s = {5,3,88,55,3,3,2,22,456,7,787,543,3,33,23,4,56,5}
+# print(s)
+# s[0] = 555  ## 'set' object does not support item assignment
+# print(s)
+########################################################
+
+# Dictionary
+
+# d = {"a":1, "b":2, "c":3, "d":4}
+# print(d)
+# print(d["a"])
+# print(d["c"])
+
+# user = {
+#     "name":"anisha",
+#     "batch":"python",
+#     "age":16,
+#     "city":"Jaipur"
+# }
+# print("User Name is",user["name"])
+# print("User Batch is",user["batch"])
+# print("Users Age is",user["age"])
+
+#############################################################
+
