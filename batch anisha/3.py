7aa23451d971a2223db6b02b15a35e98cc556644
gaurav surolia <grvsurolia@gmail.com>
2024-06-28 22:59:22 +0530

    3.py added

---
+++ batch anisha/3.py
@@ -0,0 +1,205 @@
+################# Operators
+
+# a = 2
+# b = 3
+
+# c = a + b 
+# print(c)
+
+####### Arithmetic operators
+
+# +, -, *, /, %, **, //
+
+# a = 22
+# b = 3
+
+# print(a + b)
+# print(a - b)
+# print(a * b)
+# print(a / b)
+# print(a % b)
+# print(a ** b)
+# print(a // b)
+
+######### Assignment Operators
+
+# a = 2
+
+# a = 2
+# b = 3
+# print(a)
+# print(b)
+# # a = a + b
+# a += b
+# print(a)
+# print(b)
+
+######### Comparison Operators
+
+# <, >, <=, >=, ==
+
+# a = 2
+# b = 3
+
+# print(a > b)
+# print(a < b)
+# print(a >= b)
+# print(a <= b)
+# print(a != b)
+# print(a == b)
+
+######## Logical Operators
+
+# and, or, not 
+
+# a = True
+# b = False
+
+# print(a and b)
+
+# print(a or b)
+
+# print(not a)
+
+# print(not b)
+
+############## Identity Operators
+
+# is, is not 
+
+# a = 2
+
+# print(a is not 2)
+
+############## Membership Operators
+# in, not in 
+
+# s = "My name is AnIsHa."
+
+# print("Is" in s)
+
+# print("is" not in s)
+
+# s = "gaurav"
+# S = "anisha"
+
+################ Bitwise Operators
+
+# &, |, ^, ~, <<, >>
+
+# 8
+# 1000
+# 00010000
+# print(8 << 1)
+# print(11 & 3)
+
+# 1011
+# 0011
+# 0011
+
+# print(3 | 4)
+
+# 0011
+# 0100
+
+# 0111
+
+########## 
+
+# print((2 + 3) * (5 + 15)) 
+
+##################################################
+
+# a = 2
+# b = 5
+
+# Addition is 7
+# Subtaction is -3
+# Multi is 10
+# Division is ---
+# Modulus is ---
+# Expo is ---
+# Flore division is ---
+
+############################# Type conversion
+
+# int to float 
+
+# a = 2
+# b = float(a)
+# print(a, b)
+#################### 
+# int to str 
+
+# a = 2
+# b = str(a)
+# print(a,b)
+# print(type(a), type(b))
+####################
+# int to complex
+
+# a = 2
+# b = complex(a)
+# print(a, b)
+#####################
+# float to int 
+
+# a = 2.98
+# b = int(a)
+
+# print(a, b)
+# print(type(a), type(b))
+
+#######################
+# float to string 
+
+# a = 2.33
+# b = str(a)
+#######################
+
+# str to int 
+
+# a = "anisha"
+# a = "22.44"
+# b = int(a)
+#######################
+# str to float 
+# a = "gaurav"
+# a = "2.44"
+# b = float(a)
+# print(a, b)
+# print(type(a), type(b))
+
+#######################################  USER INPUT
+
+# a = input("Enter your name - ")
+# print("Name is ", a)
+
+
+# a = input("Enter something: ")
+# print(a)
+# print(type(a))
+
+### QQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQ 
+
+# Enter your name: gaurav
+# Enter your age: 20
+# Enter your course: Python
+# Enter your city: Jaipur
+
+# Your name is gaurav, and Age is 20
+# Your course is Python, and you live in Jaipur
+
+###################################################
+### QQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQ
+
+# enter first number: 2
+# enter second number: 3
+
+# Addition is 5
+# Subtaction is -3
+# Multi is 10
+# Division is ---
+# Modulus is ---
+# Expo is ---
+# Floor division is ---
