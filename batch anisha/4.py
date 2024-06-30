6ffcc39b1bb993c8f09c858cdcbfe4a74e52baac
gaurav surolia <grvsurolia@gmail.com>
2024-06-30 22:10:13 +0530

    loops started

---
+++ batch anisha/4.py
@@ -0,0 +1,151 @@
+################## Input 
+
+# name = input("Enter your name: ")
+# print('Your name is', name)
+
+# age = int(input("Enter your age: "))
+# print("Your age is",age, " +1 = ", age+1)
+
+
+# print("aaa" + "bbb")
+# print("aaa" + "111")
+# print("20" + "1")
+# print(20 +1)
+
+####################################################
+################ Dynamic strings
+
+# name = "Anisha"
+# age = 16
+# batch = "Python"
+
+# print(f"Your name is {name}. Your age is {age}. Your batch is {batch}.")
+
+# s = f"Your name is {name}. Your age is {age}. Your batch is {batch}."
+# print(s)
+
+# print("Your name is {}. Your age is {}. Your batch is {}.".format(name, age, batch))
+
+# s = "Your name is {}. Your age is {}. Your batch is {}.".format(name, age, batch)
+
+# print(s)
+
+#######################################################################
+############## Conditions
+
+# if 
+# else 
+
+##### Syntex 
+
+# if condition:
+#     Statement
+# else:
+#     Statement
+
+##########################
+
+# a = 2
+
+# if a==2:
+#     print("Anisha")
+# else:
+#     print("Gaurav")
+
+############################ elif
+
+# if 
+# elif 
+# elif 
+# .
+# .
+# .
+# .
+# else 
+
+#### SYNTEX
+
+# if condition:
+#     Statement
+# elif condition:
+#     Statement
+# elif condition:
+#     Statement
+# .
+# .
+# .
+# .
+# else:
+#     Statement
+
+#######################
+
+# a = 3
+
+# if a==1:
+#     print("Anisha "*a)
+# elif a==2:
+#     print("Anisha "*a)
+# elif a==3:
+#     print("Anisha "*a)
+# elif a==4:
+#     print("Anisha "*a)
+# else:
+#     print("Gaurav")
+
+### QQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQ
+
+# Enter your age: 
+
+# 18 or above
+
+# You can enjoy
+# Get lost
+
+## QQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQ
+
+# Enter first number: 
+# Enter second number: 
+
+# First/Second is greater
+
+## QQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQ
+
+# Enter first number: 
+# Enter second number: 
+# Enter third number:
+
+# First/Second/Third is greater
+
+### SOLUTION
+
+# a = int(input("Enter first no: "))
+# b = int(input("Enter second no: "))
+# c = int(input("Enter third no: "))
+
+# if a>b and a>c:
+#     print("First is greater")
+# elif b>a and b>c:
+#     print("Second is greater")
+# else:
+#     print("Third is greater")
+
+#######################################################################
+
+################ LENGTH
+
+# s = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
+
+# print(len(s))
+
+# l = [2,2,2,2,2,2,2,2,2,2,2,2,2,2,3,2,2,5,2,2,77]
+# print(len(l))
+
+# t = (2,2,2,2,2,3,3,3,3,3,4,4,4,5,5,5,6,6,6,6,6,7,7,7,77)
+# print(len(t))
+
+# s = {2,3,34,4,5,5,656,7,7}
+# print(len(s))
+
+### QQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQ
+
---
+++ batch anisha/loops.py
@@ -0,0 +1,73 @@
+# Type of loops
+
+# For loop 
+# while loop 
+
+####################### FOR LOOP
+#### SYNTEX
+
+# for variable in Itrable:
+#     Statement
+##########################
+## RANGE FUNCTION
+
+# range(start, end+1, steps)
+############################
+# for i in range(1,11):
+#     print(i)
+#############################
+# l = [11,22,33,44,55,66,77,88,99]
+
+# for i in l:
+#     print(f"Value is {i}")
+
+##############################
+
+#### QQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQ
+
+# Enter a number: 33
+
+# This number is Odd / Even 
+
+#### QQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQ
+
+# Enter value: 3
+# This is Odd value 
+# Enter value: 44
+# This is Even value 
+# Enter value: 55
+# This is Odd value 
+# Enter value: 68
+# This is Even value 
+# Enter value: 87
+# This is Odd value 
+
+# for i in range(-1,-11,-1):
+#     print(i)
+
+# for i in range(1, 21, 4):
+#     print(i)
+
+#######################################
+
+# x = input("Enter a number: ")
+
+# # print(x.isdigit())
+# if not x.isdigit():
+#     print('please enter only integer')
+
+### QQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQ
+
+# Enter 10 values, it can be integer or string 
+
+# write all the integer is a single line 
+# and all strings in another single line
+# saperated with space between them 
+
+### QQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQ
+
+# Go through ASCII values at https://www.cs.cmu.edu/~pattis/15-1XX/common/handouts/ascii.html
+
+# Write a program, to take input integer from user 
+# it should not exeed the ascii values, 
+# convert the character for that value and print 
