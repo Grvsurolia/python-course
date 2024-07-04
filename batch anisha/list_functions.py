88917f334fba2c8d7db54072bcbc5f7cb560d6ef
gaurav surolia <grvsurolia@gmail.com>
2024-07-04 21:33:35 +0530

    list functions

---
+++ batch anisha/list_functions.py
@@ -0,0 +1,103 @@
+########## LIST FUNCTIONS
+
+l = [11,22,33,44,55,33,66,77,88,99,100]
+print("Initially:",l)
+# mutable 
+
+# CRUD = create, read, Update, Delete
+
+#### Append:
+
+# l.append(200)
+# print(l)
+
+#### Extend:
+
+# p = [200, 300, 400]
+# l.extend(p)
+# print(l)
+
+#### Insert: 
+
+# l.insert(3, 34)
+# print(l)
+
+#### Read
+
+# print(l[3])
+
+#### Update
+
+# l[2] = 333
+# print(l)
+
+#### Delete
+
+# l.remove(33)
+# print(l)
+
+# l.pop()
+# print(l)
+
+# l.pop(3)
+# print(l)
+
+###### Extra function
+
+#### Length of list 
+# print(len(l))
+
+##### count an element
+# c = l.count(33)
+# print(c)
+
+#### Empty list
+# l.clear()
+# print(l)
+
+#### Index
+# i = l.index(55)
+# print(i)
+
+#### Copy a list
+# p = l.copy()
+# print("p = ",p)
+# l.append(222)
+
+# print(p)
+# print(l)
+###########################
+# p = l
+# print("p = ",p)
+# l.append(222)
+
+# print(p)
+# print(l)
+
+# l.reverse()
+# print(l)
+
+# l.sort()
+# print(l)
+
+###################################################
+
+#### QQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQ
+
+# take 10 values from user, do all the list functions 
+
+#### QQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQ
+
+# Take 10 values from user, make cube of all the elements
+
+#### QQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQ
+
+# l = [11,22,33,44,55,66,77,88,99]
+# reverse the list, without using reverse function.
+
+#### QQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQ
+
+# l = [11,22,33,44,55,66,77,88,99,100]
+
+# [11,33,55,77,99]
+# [22,44,66,88,100]
