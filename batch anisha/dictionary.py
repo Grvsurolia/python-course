8a3026c00b245d5feae30939e076dcfce4ee0f3c
gaurav surolia <grvsurolia@gmail.com>
2024-07-05 22:02:06 +0530

    dict, functions, practice

--- batch anisha/dictionary.py
+++ batch anisha/dictionary.py
@@ -38,3 +38,68 @@ print(d)
 
 # print(d.values())
 
+################################################
+#### QQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQ
+
+# Enter your name: gaurav
+# Enter your age: 15
+# Enter Your batch: Python
+
+# output: {'name':gaurav, 'age': 15, 'batch':Python}
+
+#### QQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQ
+
+# questions = ['name', 'age', 'batch']
+
+# Enter your name: gaurav
+# Enter your age: 15
+# Enter Your batch: Python
+
+# output: {'name':gaurav, 'age': 15, 'batch':Python}
+
+# d = {}
+# for q in questions:
+#     i = input(f"Enter your {q}: ")
+#     d.setdefault(q,i)
+# print(d)
+
+#### QQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQ
+
+# d = {
+#     "a": 11,
+#     "b": 22,
+#     "c": 33,
+#     "d": 44
+# }
+
+# d = {
+#     "a": 111,
+#     "c": 333,
+#     "d": 444,
+#     "e":555
+# }
+
+# for i in d:
+#     zeroth_value = d[i] % 10
+#     num = (d[i] * 10) + zeroth_value
+#     d[i] = num
+# d.pop("b")
+# d["e"] = 555
+# print(d)
+
+#### QQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQ
+
+# l = ['name', 'age', 'batch']
+# t = ('gaurav',22, 'Python')
+
+# d = {'name':'gaurav', 'age': 15, 'batch':'Python'}
+
+### QQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQ
+
+# Create a dictionary for all the alphabets as KEY and there ASCII value as VALUE
+# Ex: {'a':97, 'b':98,............., 'A':65, 'B':66,...........}
+
+### QQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQ
+
+# l=  [11,3,44,5,66,6,72,71,888,8,99,2]
+# Sort this list 
---
+++ batch anisha/functions.py
@@ -0,0 +1,55 @@
+############################# FUNCTIONS
+
+##### SYNTEX
+
+# def functionName(agruments/perameters):
+#     Statement
+
+# FunctionName(agruments)
+
+##################################
+
+# def first():
+#     print("Helooooooooooooooooooooo")
+#     print("Helooo 2222222222222")
+#     print("Helooo 333333333333333")
+#     print("Helooo 44444444444444")
+
+# first()
+# first()
+# first()
+# first()
+# first()
+
+#################################
+
+# def add():
+#     a = 2
+#     b = 3
+#     print(a + b)
+
+# add()
+
+#################################
+
+#### FUNCTION WITH RETURN
+
+## SYNTEX
+
+# def functionName(agruments/perameters):
+#     Statement
+#     return element
+
+# FunctionName(agruments)
+
+########################################
+
+# def add():
+#     a = 2
+#     b = 3
+#     return a+b
+
+# c = add()
+# print(c)
+
+########################################
