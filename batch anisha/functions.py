246056972d46c3ca16b32277182ab70894b51da8
gaurav surolia <grvsurolia@gmail.com>
2024-07-08 21:11:16 +0530

    function, string function

--- batch anisha/functions.py
+++ batch anisha/functions.py
@@ -1,11 +1,11 @@
-############################# FUNCTIONS
+############################# FUNCTIONS without arguments
 
 ##### SYNTEX
 
-# def functionName(agruments/perameters):
+# def functionName():
 #     Statement
 
-# FunctionName(agruments)
+# FunctionName()
 
 ##################################
 
@@ -32,11 +32,11 @@
 
 #################################
 
-#### FUNCTION WITH RETURN
+#### FUNCTION WITH RETURN and without args
 
 ## SYNTEX
 
-# def functionName(agruments/perameters):
+# def functionName():
 #     Statement
 #     return element
 
@@ -53,3 +53,77 @@
 # print(c)
 
 ########################################
+
+############ FUNCTION WITH ARGUMENTS
+
+#### SYNTEX
+
+# def FunctionName(args):
+#     statement 
+
+# def first(a):
+#     print("Value is ",a)
+
+# # first(2)
+# first(a=2)
+
+##################
+
+# def add(a, b, c):
+#     print("Addition =",a+b+c)
+
+# add(2,4)
+# add(22,33)
+# add(a=3, b=5)
+# add(b=2, a=4)
+# add(2,3,c=4)
+
+# add(2,b=3,4) ## Not possible
+
+###################################### function with args and with return
+
+# def add(a, b):
+#     return a+b
+
+# c = add(2,3)
+# print(c)
+
+# c = add(a=2,b=3)
+# print(c)
+
+################################### FUNCTION WITH DEFAULT ARGUMENTS
+
+# def add(a=2,b=6,c=5):
+#     print("a=",a)
+#     print("b=",b)
+#     print("c=",c)
+#     return a + b + c
+
+# z = add(22, 33, 44)
+# z = add(22, 33)
+# z = add(22)
+# z = add()
+# print(z)
+
+#######################################################################
+
+#### QQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQ
+
+# Create a function "Table", take a argument as number, which print multiplication table of that number
+
+### QQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQ@
+
+# Function: add, multi, divide, subtract
+# Create a calculator
+
+### QQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQ
+
+# def p(s):
+#     print(s)
+
+# a = 22
+# p("Hello")
+# p(a)
+
+### QQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQ
+
---
+++ batch anisha/string_functions.py
@@ -0,0 +1,40 @@
+
+# print(s[0])
+# print(s[1])
+# print(s[2])
+# print(s[-1])
+# print(s[-2])
+
+# for i in s:
+#     print(i)
+
+# for i in s:
+#     if i=="e":
+#         break
+#     print(i)
+
+# name = "gaurav"
+# s = f"My name is {name}"
+# print(s)
+
+# s = "My name is {}".format(name)
+# print(s)
+
+s = "My name is Gaurav surolia."
+# print(len(s))
+# s = chr(6)
+# print(s)
+# print(s.casefold())
+# print(s.lower())
+
+# print(s.center(30, "O"))
+
+# p = "My\tname\tis\tGaurav"
+# print(p)
+
+##############################################################
+#### Assert
+
+# assert True
+# assert False
+
