49e6ad8d4ac9ebb861ded6c8f14138f8d1535615
gaurav surolia <grvsurolia@gmail.com>
2025-08-02 00:33:42 +0530

    batch 8 added

---
+++ batch 8/hello.py
@@ -0,0 +1,38 @@
+################## Print statements
+
+print("Gaurav surolia")
+print(2)
+print(3.22)
+
+################## COMMENTS
+
+# 1. This is single line comment
+"""This is multi line 
+comment, which can be used to comment
+in multiple lines."""
+
+'''This is multi line 
+comment, which can be used to comment
+in multiple lines.'''
+
+######################### data types
+
+## Numric type
+int 
+float
+
+## Sequential type
+str 
+list [1,2,3,"gaurav", 3.33, "garima"]
+tuple (1,2,3, "gaurav", 3.33, "garima")
+set {1,2,3,"gaurav",3.33, "garima"}
+
+# Mapping type
+
+dict {"key1":"value1", "key2": "value2", ............}
+
+student1 = {
+    "name": "gaurav",
+    "class": 2,
+    "roll_no": 7
+}
